7b7938f348cd66eb22d40c01de7dc08e9dfdd8bc
myst <lovedrivenbystars@gmail.com>
2025-09-20 12:31:24 -0400

    add audio

---
+++ js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+            const overlay = document.getElementById('overlay');
+            const content = document.querySelector('.box');
+            const bgMusic = document.getElementById('bg-music');
+
+            overlay.addEventListener('click', () => {
+                overlay.style.opacity = '0';
+                setTimeout(() => {
+                    overlay.style.display = 'none';
+                    content.style.display = 'flex';
+                    bgMusic.play();
+                }, 1000);
+            });
