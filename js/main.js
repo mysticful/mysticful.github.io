8eba4d4c4e4a850ff0ce5be40026007d0134bbad
myst <lovedrivenbystars@gmail.com>
2025-10-19 00:37:40 -0400

    test audio player

---
+++ js/main.js
@@ -0,0 +1,8 @@
+    const audio = document.getElementById('audioPlayer');
+    function togglePlay() {
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    }
