38d4e7611e1f61e3620fdf68e414449e641de8d4
myst <lovedrivenbystars@gmail.com>
2025-12-14 00:52:52 -0500

    yo

--- js/random-image.js
+++ js/random-image.js
@@ -2,11 +2,11 @@
     
     const images = [
       "/img/celeste.png",
-      "/img/eternalsugar.png",
       "/img/ame.png",
       "/img/chihiro.png",
       "/img/wither.png",
-      "/img/pvc.png"
+      "/img/pvc.png",
+      "/img/esc.png"
     ];
 
     const img = document.getElementById("random-img");
